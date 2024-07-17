91cec579d058797367a3ce8e0634999e1d9c3c3f
Jack Pordi <jack@katkin.com>
2024-07-17 14:47:12 +0100

    Clarify cat name formatting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('comms/welcome-fresh/:id') // TODO: different controller or routing for larger app
+  getUser(@Param() params: any): string {
+    console.log(params.id);
+    return this.appService.getUser(params.id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import * as data from '../data.json';
+
+// const userShape = {
+//   "title": "Your next delivery for <cat names, separated by comma or 'and'>",
+//   "message": "Hey <firstName>! In two days' time, we'll be charging you for your next order for <cat names, formatted as described below>'s fresh food.",
+//   "totalPrice": <total price, calculated via the formula shown in a later section in this README>,
+//   "freeGift": <true if the total price exceeds 120 pounds, otherwise false>
+// }
+
+type userData = {
+  "title": string,
+  "message": string,
+  "totalPrice": number,
+  "freeGift": Boolean,
+};
+
+const userShape = {
+  "title": "",
+  "message": "",
+  "totalPrice": 0,
+  "freeGift": false,
+};
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUser( id: string ): any {
+    let numbericId = parseInt(id, 10);
+    let user: any = data[numbericId];
+    
+    if (!user) {
+      console.log( 'user not found with numeric id', id );
+
+      user = data.find(record => record?.id === id) || {};
+    }
+
+    return user;
   }
 }
